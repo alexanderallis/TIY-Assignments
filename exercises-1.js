8d5f4c4daaee5da5dde2ff27748544719bfab090
alexanderallis <alexandreallis@gmail.com>
2016-02-08 13:43:00 -0500

    initial commit

---
+++ exercises-1.js
@@ -0,0 +1,36 @@
+ * For each of the following Array methods,
+
+ * 1. use them in an example
+ * 2. use console.log() to make the code run in the console!
+
+ **/
+
+// .sort()
+
+var array = ["5", "3", "6", "1Syntax", "Word"];
+
+array.sort();
+
+// .concat()
+
+// .indexOf()
+
+// .split()
+
+// .join()
+
+// .pop()
+
+// .push()
+
+// .slice()
+
+// .splice()
+
+// .shift()
+
+// .unshift()
+
+// .filter()
+
+// .map()
