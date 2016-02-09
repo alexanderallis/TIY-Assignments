e1b890f587c9a366da79c814a29485525b38396e
alexanderallis <alexandreallis@gmail.com>
2016-02-08 22:27:59 -0500

    fibonacci

---
+++ even-fibonacci.js
@@ -0,0 +1,11 @@
+var base = ("1");
+    first = (base+1);
+    second = (first + base);
+    third = (second + first);
+
+
+var base = ("1");
+    1 + (2/3);
+    if (console.log(output) < 4000000){
+
+    }
