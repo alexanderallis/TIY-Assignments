fd9fdc6554c7f0708781b2861d92f1e367b0dae7
alexanderallis <alexandreallis@gmail.com>
2016-02-09 10:31:55 -0500

    second class assignment

--- loop_in_class.js
+++ loop_in_class.js
@@ -1,1 +1,11 @@
 Write a for loop that will iterate from  to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (ex: "2 is even").
+
+Create a function called verbing. It should take a singel argument,
+a string,. If its length is at least 3, it shoudl add "ing" to its
+end, unless it already ends in "ing", in which case it should add "ly"
+instead. If the string length is less than 3, it should leave it
+unchanged. For example:
+
+verbing("swim"): "swimming"
+verbing("swimming"): "swimmingly"
+verbing("go"): "go"
