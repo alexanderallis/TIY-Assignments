3273afa648468830f6aa8d917355dbbabcb9ec1f
alexanderallis <alexandreallis@gmail.com>
2016-02-10 09:56:50 -0500

    Age in Class exercise

---
+++ age_in_class.js
@@ -0,0 +1,8 @@
+function calculateAge (birthYear, currentYear) {
+    birthYear = prompt("What is the year you were born?");
+    currentYear = prompt("What is the current year?");
+    "You are either " / + (currentYear - birthYear) + / "or " / + ((currentYear - birthYear) - 1) + /;
+
+}
+
+ alert("You are either " calculateAge);
