d97b437cf6d2e708f0382e2c0e5d947ae4067ff2
alexanderallis <alexandreallis@gmail.com>
2016-02-11 10:09:00 -0500

    snacks

---
+++ Snacks.js
@@ -0,0 +1,19 @@
+function calc (age, amountPerDay){
+    var total = (age - currentAge) * amountPerDay * 360;
+    console.log("You will need " + total + " to last you until the ripe old age of " + age + ".");
+}
+
+var age = prompt("How old will you be when you die?");
+var currentAge = prompt("How old are you now?")
+var amountPerDay = prompt("How many snacks do you eat per day?");
+var output = calc(age, amountPerDay);
+
+var age = prompt("How old will you be when you die?");
+var currentAge = prompt("How old are you now?")
+var amountPerDay = prompt("How many snacks do you eat per day?");
+var output = calc(age, amountPerDay);
+
+var age = prompt("How old will you be when you die?");
+var currentAge = prompt("How old are you now?")
+var amountPerDay = prompt("How many snacks do you eat per day?");
+var output = calc(age, amountPerDay);
