43628fd04dcd5c07079ca3e00625c8bf8b42af5b
alexanderallis <alexandreallis@gmail.com>
2016-02-11 11:00:27 -0500

    movie exercise

---
+++ movie.js
@@ -0,0 +1,8 @@
+var phonebook = {
+    "Title": "Kes",
+    "Duration": 113,
+    "Stars": ["David Bradley", "Lynne Perrie"]
+};
+
+
+console.log("\"" + phonebook.Title + "lasts for " + Duration + " minutes." "Stars: " + phonebook.Stars + ", and " + phonebook.Stars);
