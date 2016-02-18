513350eae4a2175c1fed55ce92b57fcab602e443
alexanderallis <alexandreallis@gmail.com>
2016-02-18 10:38:10 -0500

    grade function added

---
+++ Grade_Function.js
@@ -0,0 +1,9 @@
+var num = prompt("What is the number");
+    if (num >= 90 && num <= 100)
+        console.log("A");
+    else if (num >= 80 && num < 90)
+        console.log("B");
+    else if(num >= 70 && num < 80)
+        console.log("C");
+    else if(num < 70)
+        console.log("F");
---
+++ language_in_class.js
@@ -0,0 +1,13 @@
+var language = prompt("Choose a language");
+    if (language = "es")
+        console.log("Hola Mundo");
+
+    else if (language = "de"){
+        console.log("Hallo Welt");
+    }
+    else if (language = "fr"){
+        console.log("Bonjour tout le monde!");
+    }
+    else(){
+        console.log("You didn't type the right thing");
+    }
