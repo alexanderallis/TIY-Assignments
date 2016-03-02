62f0b85d0d673cb1a6cd711b75beb3a7e4800bc5
alexanderallis <alexandreallis@gmail.com>
2016-03-02 10:37:26 -0500

    added in class even exercise

---
+++ InClassEven21044.js
@@ -0,0 +1,32 @@
+// for(var i = 0; i <=21044; i++){
+//     if((i % 2) === 0){
+//         console.log(i + " is even");
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+for(var i = 0; i <=21044; i++){
+    if((i % 2) === 0){
+        console.log(i + " is even");
+    }
+    else{
+        console.log(i);
+    }
+}
+
+
+// even = [];
+// odd = [];
+// for(var i = 0; i <=21044; i++){
+//     if((i % 2) === 0){
+//         even.push(i);
+//     }
+//     else{
+//       odd.push(i);
+//     }
+// }
+
+// console.log("The following numbers are even: " + even);
+// console.log("The following numbers are odd: " + odd);
