46ea3da85392fd584828db777695572ba42748f2
alexanderallis <alexandreallis@gmail.com>
2016-03-07 10:55:30 -0500

    capitalize exercise

--- Capitalize-First-Letter.js
+++ Capitalize-First-Letter.js
@@ -1,10 +1,18 @@
 var string = "cry havoc and let slip the dogs of war";
 function LetterCapitalize(str){
-    var split = str.split(" ");
-    console.log(split);
+    var word = str.split(" ");
+    console.log(word);
+    console.log(word.length);
+    for(var i = 0; i<word.length; i++){
+        var unit = word[i].split("");
+        unit[0].toUpperCase();
+        console.log(unit);
 }
+}
+
 LetterCapitalize(string);
 console.log(string.length);
+fruits.unshift("Lemon","Pineapple");
 
 
 var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
