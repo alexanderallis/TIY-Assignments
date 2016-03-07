0359567f279e872ead85d23e86d01d53bb3a9a47
alexanderallis <alexandreallis@gmail.com>
2016-03-07 10:40:03 -0500

    capitalize first letter exercise

---
+++ Capitalize-First-Letter.js
@@ -0,0 +1,11 @@
+var string = "cry havoc and let slip the dogs of war";
+function LetterCapitalize(str){
+    var split = str.split(" ");
+    console.log(split);
+}
+LetterCapitalize(string);
+console.log(string.length);
+
+
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1, 3);
