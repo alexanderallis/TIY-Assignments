687df727ca00515e8903a1ea92938020a481c844
alexanderallis <alexandreallis@gmail.com>
2016-03-16 10:33:50 -0400

    country names

---
+++ countryNames.js
@@ -0,0 +1,13 @@
+var string = "";
+countryName(names){
+      var letters = names.split("");
+      for (i=0; i< letters.length; i++){
+          if(words[i] !== " "){
+          var words = [];
+          letters[i].push(words);
+          }
+          else(){
+
+          }
+      }
+}
