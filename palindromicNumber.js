d271507aa6aa13b4cc9b70ad9ab46fa8aa2b825e
alexanderallis <alexandreallis@gmail.com>
2016-03-21 10:40:45 -0400

    palindromic Number exercise

---
+++ palindromicNumber.js
@@ -0,0 +1,9 @@
+for(var i=100; i<1000; i++){
+    var cube = i;
+    for (var j = 100; j<1000; j++){
+        var product = i*j;
+        if(productArr[1,2,3] === productArr[4,5,6]){
+            console.log(productArr);
+    }
+}
+}
